2a48e59a5210c7d08277bacfb8997c99f9e79c07
Alim <alim.negmedzhan@mail.ru>
2026-03-07 02:00:01 +0500

    new landing

--- backend/apps/landing/urls.py
+++ backend/apps/landing/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import landing_view
+from .views import landing_view, auth_view
 
 urlpatterns = [
+    # Твоя текущая главная страница (index.html)
     path("", landing_view, name="landing"),
+    
+    # Новый путь для страницы входа/регистрации (auth.html)
+    path("auth/", auth_view, name="auth"),
 ]
